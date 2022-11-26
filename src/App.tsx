175831fee67a51d3c717fc01f84fd9bd39960686
Andrés Court <andres@andrescourt.com>
2022-11-25 23:05:24 -0500

    remove unnecesary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { useEffect, useState } from 'react';
-// import reactLogo from './assets/react.svg';
-import './App.css';
-
+import { useEffect } from 'react';
 function App() {
-	const [count, setCount] = useState(0);
-
 	useEffect(() => {
 		document.body.classList.add('bg-gray-100');
 	}, []);
 
 	return (
 		<div className='px-8 py-12'>
-			<h1 className="text-center font-bold text-blue-500 text-4xl">Hello World</h1>
+			<h1 className='text-center font-bold text-blue-500 text-4xl'>
+				Hello World
+			</h1>
 		</div>
 	);
 }
