db33beaeacb239530048c41e348966a616322278
Andrés Court <andres@andrescourt.com>
2022-11-25 23:04:22 -0500

    delete css from css files and started using tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
 module.exports = {
+	// purge: [],
+	darkMode: false, // or 'media' or 'class'
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#202225',
+				secondary: '#5865f2',
+				gray: colors.trueGray,
+			},
+		},
 	},
 	plugins: [],
 };
