884d5a0f7c26e6599fddba4b3b6e6c4c76fe6404
Andrés Court <andres@andrescourt.com>
2022-11-26 17:36:07 -0500

    Update Link button component

--- src/components/Buttons/LinkButton.tsx
+++ src/components/Buttons/LinkButton.tsx
@@ -1,8 +1,9 @@
 const LinkButton = (props: {text:string, buttonColor: string, textColor: string, link: string}) => {
     const { text, textColor, buttonColor, link } = props
 
+    const classToAdd = `inline-block ${buttonColor} ${textColor} px-8 py-2 mt-3 rounded-lg hover:bg-blue-300 hover:text-gray-700 shadow-lg hover:shadow-none uppercase tracking-wide font-bold`
 
-    return <a className="btn-primary" href={link}>{text}</a>
+    return <a className={classToAdd} href={link}>{text}</a>
 }
 
 export { LinkButton }
